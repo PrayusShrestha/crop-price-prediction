714d1c038d50902f7ffcf0dbd2e55fa480a4b7c4
Ronin Cunningham <ronincunningham1@gmail.com>
2023-04-23 20:30:22 -0700

    Create RNN and GRU models, train and test them.

---
+++ src/gru_model.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+# Define the GRU model
+class GRUModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(GRUModel, self).__init__()
+        self.gru = nn.GRU(input_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x, _ = self.gru(x)
+        x = self.fc(x)
+        return x
---
+++ src/rnn_model.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+# Define the basic RNN model
+class RNNModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNNModel, self).__init__()
+        self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x, _ = self.rnn(x)
+        x = self.fc(x)
+        return x
